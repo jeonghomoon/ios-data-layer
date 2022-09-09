d8691f7ccf6fb957f80cc64a4e59b683c9516867
jeonghomoon <113110283+jeonghomoon@users.noreply.github.com>
2022-09-10 07:35:54 +0900

    feat: create Alamofire router  (#9)
    Alamofire의 URLRequestConvertible 프로토콜을 채택한 Routable 프로토콜을 작성하였습니다.
    Routable 프로토콜은 다음과 같은 프로퍼티를 가집니다.
    
     - baseURL: API 서버가 하나인 경우, default implementation을 이용해 추가 작성할 필요가 없습니다.
     - method, path, parameters
     - asURLRequest: baseURL에 path를 append한 URL을 만듭니다, RESTFul 하지않은 요청의 경우 Error을 던집니다.
    
    Routable 프로토콜에 encoding, multipartFormData 프로퍼티를 추가하였습니다.
    encoding 프로퍼티의 경우, RESTFul하지 않은 요청에 대한 처리를 가능하게 합니다.
    multipartFormData 프로퍼티의 경우, multipartFormData를 이용한 upload 요청을 가능하게 합니다.
    
    Refs: #7

---
+++ DataLayer/Alamofire/Routable.swift
@@ -0,0 +1,77 @@
+//
+//  Routable.swift
+//  DataLayer
+//
+//  Created by Jeongho Moon on 2022/09/10.
+//
+
+import Alamofire
+
+protocol Routable: URLRequestConvertible {
+    var baseURL: URL { get }
+
+    var method: HTTPMethod { get }
+
+    var path: String { get }
+
+    var parameters: Parameters { get }
+
+    var encoding: ParameterEncoding? { get }
+
+    var multipartFormData: MultipartFormData? { get }
+}
+
+extension Routable {
+    var baseURL: URL {
+        URL(string: "localhost:8080")!
+    }
+
+    var parameters: Parameters {
+        Parameters()
+    }
+
+    var encoding: ParameterEncoding? {
+        nil
+    }
+
+    var multipartFormData: MultipartFormData? {
+        nil
+    }
+
+    func asURLRequest() throws -> URLRequest {
+        let url = baseURL.appendingPathComponent(path)
+        var request = URLRequest(url: url)
+        request.method = method
+
+        if let encoding = encoding {
+            request = try encoding.encode(request, with: parameters)
+        } else {
+            let encoding: ParameterEncoding
+
+            switch method {
+            case .post, .put:
+                encoding = JSONEncoding.default
+            case .get, .delete:
+                encoding = URLEncoding.default
+            default:
+                debugPrint(
+                    """
+                        Failed to endcode parameters, Check if API is RESTful,
+                        Parameters is encodable, or Method is valid.
+                    """
+                )
+
+                throw NetworkServiceError.parametersEncodingFailed
+            }
+
+            request = try encoding.encode(request, with: parameters)
+        }
+
+        return request
+    }
+}
+
+
+enum NetworkServiceError: Error, Equatable {
+    case parametersEncodingFailed
+}
